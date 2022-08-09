8707750ce15ff99a52699f30e0562129b04d0bd5
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2022-08-09 14:13:13 +0000

    Added tests

---
+++ build/test/CMakeFiles/Example.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/home/nil000/Projects/RPN/ez_interpv/test/Example.f90" "/home/nil000/Projects/RPN/ez_interpv/build/test/CMakeFiles/Example.dir/Example.f90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "Intel")
+set(CMAKE_Fortran_SUBMODULE_SEP "@")
+set(CMAKE_Fortran_SUBMODULE_EXT ".smod")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_Fortran
+  "HAVE_RMN"
+  "HAVE_VGRID"
+  "Little_Endian"
+  )
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  "."
+  "test/../src"
+  "/home/ords/cmdd/cmds/nil000/ssm/workspace/rmn/include"
+  "/home/ords/cmdd/cmds/nil000/ssm/workspace/vgrid/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nil000/Projects/RPN/ez_interpv/build/src/CMakeFiles/ezinterpv90.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/test/CMakeFiles/TestVI.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for TestVI.
+# This may be replaced when dependencies are built.
